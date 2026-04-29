5999e5df76c1368473e52146b0d389b0bb578c4a
neojarvis <techadmin@iamneo.ai>
2026-04-29 13:25:44 +0000

    Manual-save-commit-4/29/2026--6:55:43-PM

---
+++ Project/main.py
@@ -0,0 +1,24 @@
+from student_functions import add_student,view_students,filter_by_department
+from course_functions import add_course,view_courses
+from enrollment_functions import enroll_student
+
+def menu():
+    while True:
+        print(\n1. Add Student)
+        print("2. View Studnets by Department")
+        print("3. Add Course")
+        print("4. Enroll Student")
+        print("5. Exit")
+
+        choice=input("Enter your choice: ").strip()
+        if choice=="1":
+            try:
+                student_id=int(input("Enter Student ID: "))
+                name=input("Enter Name: ")
+                age=int(input("Enter Age: "))
+                department=input("Enter Department: ")
+                add_student(student_id,name,age,department)
+                print(f"Student {name} added successfully.")
+            except ValueError as e:
+                print(f"Error: {e}")
+                
