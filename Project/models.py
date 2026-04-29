9c08253ad95314425fb3264c35d4a02647733db4
neojarvis <techadmin@iamneo.ai>
2026-04-29 13:13:55 +0000

    Auto-save-commit-4/29/2026--6:43:53-PM

---
+++ Project/models.py
@@ -0,0 +1,26 @@
+class Student:
+    def __init__(self,student_id,name,age,department):
+        self.student_id=student_id
+        self.name=name
+        self.age=age
+        self.department=department
+    def get_info(self):
+        return f"Student(ID:{self.student_id}, Name:{self.name}, Age:{self.age}, Department:{self.department})"
+
+class Course:
+    def __init__(self,course_id,name,credits):
+        self.course_id=course_id
+        self.name=name
+        self.credits=credits
+    def get_info(self):
+        return f"Course: {self.name}, Credits:{self.credits}"
+    def __str__(self):
+        return f"Course(ID:{self.course_id}, Name:{self.name}, Credits:{self.credits})"
+class Enrollment:
+    def __init__(self,student,course):
+        self.student=student
+        self.course=course
+    def get_info(self):
+        return f"Student: {self.student.name}, Course: {self.course.name}"
+    def __str__(self):
+        return f"E"
